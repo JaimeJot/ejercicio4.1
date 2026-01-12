fbe7736c56dd30b1c6b332f4bbfd3daf551cd57f
Jaime <jaimeacicuendez@gmail.com>
2026-01-12 12:57:42 +0100

    saludo con caja de texto

--- js/paginasimple.js
+++ js/paginasimple.js
@@ -1,10 +1,16 @@
 window.onload = function() {
     const texto = document.getElementById("texto");
     const saludos = document.getElementsByName("saludo");
-
+    let nombre = document.getElementById("nombre");
+    let boton = document.getElementById("boton");
     Array.from(saludos).forEach(saludo => {
         saludo.addEventListener("click", function() {
             texto.textContent = saludo.value;
         });
     });
+    boton.onclick = mostrarnombre;
+    function mostrarnombre(){
+        texto.textContent = "Hola " + nombre.value;
+    }
 }
+
